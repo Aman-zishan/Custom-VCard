258ba5c6c674af14f6a9270159dd445933c1993a
amanzishan <amanzishan.az@gmail.com>
2020-08-06 13:29:07 +0530

    add

--- core/admin.py
+++ core/admin.py
@@ -4,3 +4,6 @@ from .models import Profile, Gallery
 
 admin.site.register(Profile)
 admin.site.register(Gallery)
+admin.site.site_header = "E-bizCard Admin"
+admin.site.site_title = "E-BizCard Admin Portal"
+admin.site.index_title = "Welcome to E-BizCard Admin Portal"
